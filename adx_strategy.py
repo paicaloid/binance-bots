e72480d9ed810495e44ca7f17631b8b194d0b78a
paicaloid <paicaloid@gmail.com>
2023-05-11 00:02:51 +0700

    Verify stoch_rsi

---
+++ adx_strategy.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import vectorbtpro as vbt
+
+from pydantic import BaseSettings
+
+from indicators.adx import ADX
+
+class adxSetting(BaseSettings):
+    adx_lenght : int = 14
+    adx_level : int = 20
+    adx_diff : float = 1.0
+
+class emaSetting(BaseSettings):
+    ema_length : int = 20
+    ema_short_length : int = 10
+
+class rsiSetting(BaseSettings):
+    rsi_lenght : int = 14
+    overbought_level : int = 70
+    oversold_level : int = 30
+
+class stochasticSetting(BaseSettings):
+    smooth_k : int = 3
+    smooth_d : int = 3
+    stochastic_lenght : int = 14
+    overbought_level : int = 80
+    oversold_level : int = 20
+
+class stopSetting(BaseSettings):
+    stop_loss : float = 0.03
+    take_profit : float = 0.16
+    trail_stop_activate : float = 0.01
+    trail_stop_execute : float = 0.001
+
+class Strategy:
+
+    def __init__(
+        self,
+        adx_setting : adxSetting = adxSetting(),
+        ema_setting : emaSetting = emaSetting(),
+        rsi_setting : rsiSetting = rsiSetting(),
+        stochastic_setting : stochasticSetting = stochasticSetting(),
+        sl_tp_ts_setting : stopSetting = stopSetting(),
+    ):
+        self.adx_setting = adx_setting
+        self.ema_setting = ema_setting
+        self.rsi_setting = rsi_setting
+        self.stochastic_setting = stochastic_setting
+        self.sl_tp_ts_setting = sl_tp_ts_setting
+
+    def compute_signal(
+        self,
+        close_price: pd.Series,
+        high_price: pd.Series,
+        low_price: pd.Series,
+    ):
+        adx, plusDI, minusDI = ADX(
+            high=high_price,
+            low=low_price,
+            close=close_price,
+            period=self.adx_setting.adx_lenght,
+        )
+
+        self.adx = adx.iloc[-1]
+        self.plusDI = plusDI.iloc[-1]
+        self.minusDI = minusDI.iloc[-1]
+
+        stoch_rsi = vbt.pandas_ta("STOCHRSI").run(
+            close_price, 
+            length=self.stochastic_setting.stochastic_lenght, 
+            rsi_length=self.rsi_setting.rsi_lenght,
+            k=self.stochastic_setting.smooth_k,
+            d=self.stochastic_setting.smooth_d,
+        )
+        self.k = stoch_rsi.stochrsid.iloc[-1]
+        self.d = stoch_rsi.stochrsik.iloc[-1]
+
---
+++ backtest.py
@@ -0,0 +1,97 @@
+import pytz
+import pandas as pd
+import vectorbtpro as vbt
+
+from datetime import datetime, timedelta
+from typing import Union
+
+from adx_strategy import Strategy
+
+class Backtest():
+
+    def __init__(
+        self, 
+        symbol: str,
+        end_date: datetime, 
+        start_date: Union[datetime, None] = None ,
+        bar_range: Union[int, None] = 100,
+        window_size: int = 250,
+        time_frame: str = "1m",
+        cash: float = 1000.0
+    ) -> None:
+        
+        self.symbol = symbol
+        self.window_size = window_size
+        self.end_date = end_date
+        self.bar_range = bar_range
+        self.time_frame = time_frame
+        
+        if isinstance(start_date, datetime):
+            if time_frame == '1m':
+                self.start_date = start_date - timedelta(minutes=self.window_size)
+                self.bar_range = int((end_date - start_date).total_seconds() / 60.0)
+            elif time_frame == '1h':
+                self.start_date = start_date - timedelta(hours=self.window_size)
+                self.bar_range = int((end_date - start_date).total_seconds() / 3600.0)
+        else:
+            if time_frame == '1m':
+                self.start_date = self.lastest_date - timedelta(minutes=self.bar_range + self.window_size)
+            elif time_frame == '1h':
+                self.start_date = self.lastest_date - timedelta(hours=self.bar_range + self.window_size)
+
+        self.port = vbt.pf_enums.ExecState(
+            cash=cash,
+            position=0.0,
+            debt=0.0,  
+            locked_cash=0.0,  
+            free_cash=cash,
+            val_price=0.0,
+            value=0.0
+        )
+
+    def get_historical_data(self) -> pd.DataFrame:
+        if self.time_frame == '1m':
+            timeframe = '1 minutes'
+        elif self.time_frame == '1h':
+            timeframe = '1 hours'
+    
+        data = vbt.BinanceData.fetch(
+            self.symbol.upper(),
+            start=self.start_date,
+            end=self.end_date,
+            timeframe=timeframe,
+            tz="Asia/Bangkok",
+        )                      
+        self.data = data.data[self.symbol.upper()]
+
+    def simulate(self):
+
+        self.strategy = Strategy()
+
+        for index in range(1, self.bar_range + 1):
+            data = self.data[index:index + self.window_size]
+
+            self.strategy.compute_signal(
+                close_price=data["Close"],
+                high_price=data["High"],
+                low_price=data["Low"],
+            )
+
+            print()
+
+
+
+if __name__ == '__main__':
+    start_date = datetime.strptime("2023-05-05 00:00:00", "%Y-%m-%d %H:%M:%S")
+    end_date = datetime.strptime("2023-05-10 21:00:00", "%Y-%m-%d %H:%M:%S")
+    sim = Backtest(
+        symbol="opusdt", 
+        start_date=start_date,
+        end_date=end_date,
+        bar_range=239,
+        window_size=250,
+        time_frame="1h",
+    )
+    sim.get_historical_data()
+    sim.simulate()
+    print()
