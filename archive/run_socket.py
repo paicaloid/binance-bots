7bf4d9dcd8f014ecd59e3d731ec1433bf8363bc6
paicaloid <paicaloid@gmail.com>
2023-07-13 11:43:28 +0700

    Add archive

--- archive/run_socket.py
+++ archive/run_socket.py
@@ -1,9 +1,11 @@
 import asyncio
 import json
-# import pandas as pd
+import pandas as pd
 import pytz
 # import vectorbtpro as vbt
 import websockets
+
+from utils import get_historical_data, msg_to_dataframe
 
 
 class BinanceWebsocketHandler:
@@ -11,7 +13,6 @@ class BinanceWebsocketHandler:
         self,
         symbol: str = "btcusdt",
         interval: str = "1m",
-        trade: bool = True,
         bar_range: int = 250,
         sql: bool = True
     ) -> None:
@@ -24,35 +25,45 @@ class BinanceWebsocketHandler:
         self.connections = []
         self.connections.append(f'wss://stream.binance.com:9443/ws/{self.symbol}@kline_{interval}')
 
-        if trade:
-            pass
+        self.data = get_historical_data(
+            symbol=self.symbol,
+            interval=self.intreval,
+            bar_range=self.bar_range
+        )
+
+    def update_dataframe(self, lastest_df: pd.DataFrame) -> None:
+        self.data = pd.concat([self.data, lastest_df])
+        self.data = self.data[-self.bar_range:]
 
     async def handle_kline_1m_message(self, message) -> None:
         msg = json.loads(message)
         bar = msg['k']
         is_close = bar['x']
-        print(bar)
+        # print(bar)
         if is_close:
-            print(bar)
+            df = msg_to_dataframe(info=bar, interval=self.intreval, tz=self.tz)
+            self.update_dataframe(df)
+            print(self.data)
 
     async def handle_socket(self, uri):
         async with websockets.connect(uri) as websocket:
             if 'kline_1m' in uri:
                 handle_message = self.handle_kline_1m_message
-            elif 'kline_1s' in uri:
-                handle_message = self.handle_kline_1s_message
+            elif 'kline_1h' in uri:
+                handle_message = self.handle_kline_1h_message
+
             async for message in websocket:
                 await handle_message(message)
 
     async def run(self):
-        await asyncio.wait([self.handle_socket(uri) for uri in self.connections])
+        await asyncio.gather(*[self.handle_socket(uri) for uri in self.connections])
 
 
 if __name__ == '__main__':
     handler = BinanceWebsocketHandler(
         symbol='opusdt',
         interval='1m',
-        trade=False,
+        # trade=True,
         bar_range=250,
         sql=False
     )
---
+++ archive/utils.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import pytz
+import vectorbtpro as vbt
+from datetime import datetime, timedelta
+
+
+def msg_to_dataframe(info: dict, interval: str, tz: pytz.timezone) -> pd.DataFrame:
+    cur_datetime = datetime.fromtimestamp(info['t']/1000, tz=tz)
+    print(cur_datetime)
+    df = pd.DataFrame([info])
+    df = df.drop(['T', 't', 's', 'i', 'f', 'L', 'x', 'B'], axis=1)
+    df = df.rename(columns={
+        'o': 'Open',
+        'h': 'High',
+        'l': 'Low',
+        'c': 'Close',
+        'v': 'Volume',
+        'q': 'Quote volume',
+        'n': 'Trade count',
+        'V': 'Taker base volume',
+        'Q': 'Taker quote volume'
+    })
+    df = df.astype(float)
+    df["Trade count"] = df["Trade count"].astype(int)
+
+    df['Datetime'] = cur_datetime
+    df = df.set_index('Datetime')
+
+    # if self.sql:
+    #     df.to_sql(f'btcusdt_{interval}', engine, if_exists='append')
+
+    return df
+
+
+def get_historical_data(
+    symbol: str,
+    interval: str,
+    bar_range: int,
+) -> pd.DataFrame:
+    print("Preparing data...")
+
+    date_now = datetime.now(tz=pytz.timezone('Asia/Bangkok')).replace(second=0, microsecond=0)
+    if interval == '1m':
+        start_date = date_now - timedelta(minutes=bar_range)
+        timeframe = '1 minutes'
+    elif interval == '1h':
+        start_date = date_now - timedelta(hours=bar_range)
+        timeframe = '1 hours'
+    else:
+        raise ValueError(f'Interval {interval} is not supported.')
+
+    data = vbt.BinanceData.fetch(
+        symbol.upper(),
+        start=start_date,
+        end=date_now,
+        timeframe=timeframe,
+        tz="Asia/Bangkok",
+    )
+    data = data.data[symbol.upper()]
+    data.index = data.index.rename('Datetime')
+
+    return data
