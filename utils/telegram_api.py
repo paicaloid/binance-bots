e7fb33c1a083e0f1748677016c0aaf9d551b001e
paicaloid <paicaloid@gmail.com>
2023-12-14 16:17:34 +0800

    Add user data streaming functionality

--- utils/telegram_api.py
+++ utils/telegram_api.py
@@ -10,13 +10,13 @@ load_dotenv(
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 CHAT_ID = os.getenv("CHAT_ID")
-print(TELEGRAM_TOKEN, CHAT_ID)
+# print(TELEGRAM_TOKEN, CHAT_ID)
 base_url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/"
 
 
 def send_message(msg: str):
     url = f"{base_url}sendMessage?chat_id={CHAT_ID}&text={msg}"
     response = requests.get(url)
-    print(response)
+    # print(response)
     if response.status_code != 200:
         raise Exception("Telegram API Error")
